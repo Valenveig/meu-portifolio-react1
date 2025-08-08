2805d4293cfa56255d67daa320b7726393c345df
Valentina Gomes <valegod1238@gmail.com>
2025-08-08 14:36:38 -0300

    Corrige caminho base para deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/meu-portifolio-react1/',
+  base: '/meu-portifolio-react1/', // <-- nome do repositório
   plugins: [react()],
 });
